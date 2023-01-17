60d1056b178eb706c6fae932e3910d702571d223
Arthur Oxenhorn <arthuro@cs.huji.ac.il>
2023-01-17 19:20:15 +0200

    merging

--- components/dropdown.js
+++ components/dropdown.js
@@ -1,6 +1,5 @@
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Dropdown from 'react-bootstrap/Dropdown';
-import { DropdownButton } from 'react-bootstrap';
 import Image from 'next/image';
 import TransLogo from '../resources/translation_logo.svg'
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -9,27 +8,27 @@ import React, { useState } from 'react';
 
 export default function DropDown({ children, handleSubmit }) {
 
-return (
-    // <select id="dropDownSelector" onChange={handleSubmit}></select>
-    <Dropdown onSelect={handleSubmit}>
-      <Dropdown.Toggle variant="success" id="dropdown-basic"  className='lang-btn'>
-        <Image className='custom-lang-btn-image' alt='translations-logo'
-          src={TransLogo}
-          width={42.61}
-          height={32.76}>
-        </Image>
-      </Dropdown.Toggle>
-      <Dropdown.Menu className='custom-ddm'>
-        {children.primaryLanguages.map(item => (
-            <Dropdown.Item className='custom-ddm-item' eventKey={item} key={item['code']}>{item['native_name']}</Dropdown.Item>
-        ))}
-        <Dropdown.Divider />
-        {children.secondaryLanguages.map(item => (
-            <Dropdown.Item className='custom-ddm-item' eventKey={item} key={item['code']}>{item['native_name']}</Dropdown.Item>
-        ))}
-      </Dropdown.Menu>
-    </Dropdown>
-  );
+    return (
+        // <select id="dropDownSelector" onChange={handleSubmit}></select>
+        <Dropdown onSelect={handleSubmit}>
+            <Dropdown.Toggle variant="success" id="dropdown-basic" className='lang-btn'>
+                <Image className='custom-lang-btn-image' alt='translations-logo'
+                    src={TransLogo}
+                    width={42.61}
+                    height={32.76}>
+                </Image>
+            </Dropdown.Toggle>
+            <Dropdown.Menu className='custom-ddm'>
+                {children.primaryLanguages.map(item => (
+                    <Dropdown.Item className='custom-ddm-item' eventKey={item} key={item['code']}>{item['native_name']}</Dropdown.Item>
+                ))}
+                <Dropdown.Divider />
+                {children.secondaryLanguages.map(item => (
+                    <Dropdown.Item className='custom-ddm-item' eventKey={item} key={item['code']}>{item['native_name']}</Dropdown.Item>
+                ))}
+            </Dropdown.Menu>
+        </Dropdown>
+    );
 
     // return (
     //     <label>
