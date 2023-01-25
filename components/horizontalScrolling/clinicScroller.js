1ad65023e3e8befc513db339667b92b94aa249d3
Arthur Oxenhorn <arthuro@cs.huji.ac.il>
2023-01-25 13:08:11 +0200

    align title

--- components/horizontalScrolling/clinicScroller.js
+++ components/horizontalScrolling/clinicScroller.js
@@ -44,16 +44,25 @@ export default function ClinicScroller({ actions, textData }) {
     const contentStyle = () => {
         if (textDirection == 'rtl') {
             return {
-                'text-align': 'center',
-                'height': 'fit-content',
-                'margin-bottom': '15px',
                 'direction': 'rtl',
             }
         } else {
             return {
-                'text-align': 'center',
-                'height': 'fit-content',
-                'margin-bottom': '15px'
+            }
+        }
+    }
+
+    const titleStyle = () => {
+        if (textDirection == 'rtl') {
+            return {
+                'direction': 'rtl',
+                'textAlign': 'right',
+                'margin-right': "1em"
+            }
+        } else {
+            return {
+                'textAlign': 'left',
+                'margin-left': "1em"
             }
         }
     }
@@ -66,7 +75,7 @@ export default function ClinicScroller({ actions, textData }) {
     return (
         <>
             <hr className='separator' />
-            <div className='card-header-text'>{textData.cardHeaderText}</div>
+            <div className='card-header-text' style={titleStyle()}>{textData.cardHeaderText}</div>
             <Swiper
                 dir={textDirection}
                 slidesPerView={'auto'}
@@ -83,7 +92,7 @@ export default function ClinicScroller({ actions, textData }) {
                             onClick={() => clickedSlide(item.clinic.id, item.clinic.name)}
                             style={slideStyle()}>
                             <div className={(item.clinic.id != slide) ? "text-in-card text-in-card-reg" : "text-in-card text-in-card-selected"}>
-                                <span className='text-in-card-title'>{item.clinic.name}</span>
+                                <span className='text-in-card-title' >{item.clinic.name}</span>
                             </div>
                             <div className={(item.clinic.id != slide) ? "desc-in-card text-in-card-reg" : "desc-in-card text-in-card-selected"}>
                                 {item.clinic.description}
